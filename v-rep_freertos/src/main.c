2f0a6000c2c6f7938fc3bdc407230919ba4c3a51
wsk <1653484428@qq.com>
2022-09-08 21:21:47 +0800

    control joint by velocity and position

--- v-rep_freertos/src/main.c
+++ v-rep_freertos/src/main.c
@@ -18,14 +18,18 @@
 #include <math.h>
 #include "extApi.h"
 
+#define PI 3.1415926
+
 int main()
 {
 	int Port = 3000;
 	int PositionControlHandle;
-	int joint3Handle;
+	int joint1Handle;
+	int objectHandle;
 	int state = 0;
-	float lv[3] = {0,0,0};
-	float av[3] = {0,0,0};
+	simxFloat linev[3] = {2,2,2};
+	simxFloat anglev[3] = {1,1,1};
+	float joint1_position;
 	float position[3];
 	float positionmove[3];
 	//智能制造单元控制系统 Intelligent manufacturing unit control system
@@ -41,18 +45,21 @@ int main()
 		while (simxGetConnectionId(clientID) != -1)
 		{
 			count++;
-			simxGetObjectHandle(clientID, "IRB140_manipulationSphere", &PositionControlHandle, simx_opmode_oneshot);
-			state = simxGetObjectPosition(clientID, PositionControlHandle, -1, position, simx_opmode_blocking);
-			positionmove[0] = position[0];
-			positionmove[1] = position[1] +0.1 * sin(count / 10.0);
-			positionmove[2] = position[2];
-			simxSetObjectPosition(clientID, PositionControlHandle, -1, positionmove, simx_opmode_oneshot);
-			printf("(%f,%f,%f)\r\n", position[0], position[1], position[2]);
-			//simxGetObjectHandle(clientID, "IRB140_joint3", &joint3Handle, simx_opmode_oneshot);
-			//state = simxSetJointTargetPosition(clientID, &joint3Handle, 90, simx_opmode_oneshot);
-			//simxGetObjectPosition(clientID, &joint3Handle, -1, &av, simx_opmode_oneshot);
-			//printf("(%f,%f,%f)\n", av[0], av[1], av[2]);
-			//printf("%d\n", state);
+			//simxGetObjectHandle(clientID, "IRB140_manipulationSphere", &PositionControlHandle, simx_opmode_oneshot);
+			//state = simxGetObjectPosition(clientID, PositionControlHandle, -1, position, simx_opmode_blocking);
+			//positionmove[0] = position[0];
+			//positionmove[1] = position[1] +0.01 * sin(count / 10.0);
+			//positionmove[2] = position[2];
+			//simxSetObjectPosition(clientID, PositionControlHandle, -1, positionmove, simx_opmode_oneshot);
+			//printf("(%f,%f,%f)\r\n", position[0], position[1], position[2]);
+			simxGetObjectHandle(clientID, "IRB140_joint1", &joint1Handle, simx_opmode_oneshot);
+			simxGetJointPosition(clientID, joint1Handle, &joint1_position, simx_opmode_oneshot);
+			simxSetJointTargetPosition(clientID, joint1Handle, joint1_position + 0.1*PI, simx_opmode_oneshot);
+
+			/*关节速度控制*/
+			//state = simxSetJointTargetVelocity(clientID, joint1Handle, 0.2 * PI, simx_opmode_oneshot);
+			//simxGetObjectVelocity(clientID, joint1Handle, linev, anglev, simx_opmode_oneshot);
+			//printf("(%f,%f,%f) (%f, %f, %f)\n", anglev[0], anglev[1], anglev[2], linev[0], linev[1], linev[2]);
 
 		}
 
